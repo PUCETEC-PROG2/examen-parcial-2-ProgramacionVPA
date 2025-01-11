299304a206d4579867c5fdb0865266156980e1eb
Victor Alvarez <VPALVAREZ@puce.edu.ec>
2025-01-11 10:52:30 -0500

    examen 2

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render, redirect
+from .models import Movie
 
 # Create your views here.
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'movies': movies}, request))
+
+
+def movie_detail(request, movie_id):
+    movies = Movie.objects.get(pk = movie_id)
+    template = loader.get_template('detail.html')
+    context = {
+        'movies': movies
+    }
+    return HttpResponse(template.render(context, request))
